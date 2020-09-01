b4cabc9e807bc95cbafb86084833834567fe947c
Rachael Graveling <clrg092@cl.glow.scot>
2020-09-01 10:29:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Zoo records
+#Rachael Graveling
+#01/09/2020
+
+
+animal = []
+all_animals = []
+
+for i in range (2):
+  animal.append (input("Animal: "))
+  animal.append (input("Endangered (Yes or No): "))
+  animal.append (input("Life span: "))
+  animal.append (input("Continent: "))
+  animal.append (input("Enclosure: "))
+
+  all_animals.append(animal)
+
+
+
+
+
+
